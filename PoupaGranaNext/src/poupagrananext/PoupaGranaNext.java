c94396b9426cc8e2cd58ae32311cbd338303480d
Valter <valteralvees@outlook.com>
2022-06-01 13:41:08 -0300

    Added class

---
+++ PoupaGranaNext/src/poupagrananext/PoupaGranaNext.java
@@ -0,0 +1,18 @@
+package poupagrananext;
+import java.util.Scanner;
+
+public class PoupaGranaNext {
+
+    public static void main(String[] args) {
+        // Teclado
+        Scanner teclado = new Scanner(System.in);
+        
+        //Apresentação e escolha da funcionalidade
+        System.out.println("===============Bem vindo ao Next!=================== ");
+        System.out.println("1- DEPOSITAR");
+        System.out.println("2- RETIR");
+        
+        
+    }
+    
+}
